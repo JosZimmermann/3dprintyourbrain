b41434d8de8744b641ce24d659406685e5fe4b0c
JosZimmermann <48101657+JosZimmermann@users.noreply.github.com>
2024-11-08 10:34:46 +0100

    Update combine_mesh.py

--- T1_to_3Dprint/combine_mesh.py
+++ T1_to_3Dprint/combine_mesh.py
@@ -3,7 +3,7 @@ import sys
 
 # Check if the correct number of arguments is provided
 if len(sys.argv) != 4:
-    print("Usage: python3 smooth_mesh.py <input_mesh1> <input_mesh2> <output_mesh>")
+    print("Usage: python3 scale_mesh.py <input_mesh1> <input_mesh2> <output_mesh>")
     sys.exit(1)
 
 # Retrieve the file paths from command-line arguments
@@ -17,16 +17,13 @@ ms = pymeshlab.MeshSet()
 # Load the initial mesh (first input mesh)
 ms.load_new_mesh(initial_mesh_path)
 
-# Load the additional mesh (second input mesh) and merge it into the MeshSet
+# Load the additional mesh (second input mesh)
 ms.load_new_mesh(additional_mesh_path)
 
-# Flatten all the layers (merge them into one mesh)
-ms.flatten_visible_layers()
+# Apply filters to clean up the meshes
+ms.apply_filter('meshing_remove_duplicate_vertices')       # Remove duplicate vertices
+ms.apply_filter('meshing_remove_unreferenced_vertices')    # Remove unreferenced vertices
+#ms.apply_filter('meshing_remove_connected_component_by_face_number', mincomponentsize =25)  # Keep only the largest component
 
-# Apply filters to clean up the merged mesh
-ms.apply_filter('remove_duplicate_vertices')       # Remove duplicate vertices
-ms.apply_filter('remove_unreferenced_vertices')    # Remove unreferenced vertices
-ms.apply_filter('remove_isolated_pieces', mincomponent=0)  # Keep only the largest component
-
-# Save the cleaned, merged mesh as a binary STL
+# Save the merged mesh as a binary STL
 ms.save_current_mesh(output_mesh_path, binary=True)
